1e1cc55885a7b3fc0520cea4c2a8b7924c85c754
Min Dong <anniemindong@gmail.com>
2021-01-25 19:21:47 -0800

    microservice Eureka

--- ClientService/src/main/java/com/antra/report/client/MainClientApplication.java
+++ ClientService/src/main/java/com/antra/report/client/MainClientApplication.java
@@ -4,9 +4,11 @@ import com.amazonaws.services.sqs.AmazonSQSAsync;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MainClientApplication {
     @Bean
     public QueueMessagingTemplate queueMessagingTemplate(
